fef86c44b6d2e22f3f6f302eebb17344a41b0c25
chenweiqi2 <chenweiqi2@tal.com>
2025-05-15 20:57:32 +0800

    调整kotlin编译版本

--- CloudPlugin/build.gradle.kts
+++ CloudPlugin/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation(libs.kotlin)
-    compileOnly("com.android.tools.build:gradle:8.3.0")
+    compileOnly("com.android.tools.build:gradle:8.0.0")
     implementation(gradleApi())
     implementation("org.ow2.asm:asm:9.6")
     implementation("org.ow2.asm:asm-commons:9.6")
